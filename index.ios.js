9c91a50095a3dd275e8a2f4f3c76cb70783406e4
Diane Liu <dianeliu@u.northwestern.edu>
2017-01-13 13:58:25 -0600

    init firebase

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCZLWVwyg4dnTcWWotXpqYfn-RF02mX4r4",
+  authDomain: "pairachute-5adf4.firebaseapp.com",
+  databaseURL: "https://pairachute-5adf4.firebaseio.com",
+  storageBucket: "pairachute-5adf4.appspot.com",
+  messagingSenderId: "803241803289"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class Pairachute extends Component {
   render() {
