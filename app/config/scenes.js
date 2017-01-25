41e645d931b1f57799725b9b2275bc421e77ff75
Diane Liu <dianeliu@u.northwestern.edu>
2017-01-25 02:57:30 -0600

    add HomeContainer

--- app/config/scenes.js
+++ app/config/scenes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Actions, Scene, Switch } from 'react-native-router-flux';
 import LoginContainer from '../components/auth/LoginContainer';
-import Home from '../components/home/Home';
+import HomeContainer from '../components/home/HomeContainer';
 import SignupContainer from '../components/auth/SignupContainer';
 
 
@@ -16,7 +16,7 @@ const scenes = Actions.create(
       initialScene={true}
     >
       <Scene key="login" component={LoginContainer} hideNavBar={true}/>
-      <Scene key="home" component={Home} hideNavBar={true}/>
+      <Scene key="home" component={HomeContainer} hideNavBar={true}/>
     </Scene>
     <Scene key="signup" component={SignupContainer}/>
   </Scene>
