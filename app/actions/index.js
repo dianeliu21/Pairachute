5016b30572052c4e80b982f06e90405a6c7347dd
Diane Liu <dianeliu@u.northwestern.edu>
2017-02-21 13:28:51 -0600

    onboarding files

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,7 @@
 import * as AuthActions from './authActions';
+import * as SettingsActions from './settingsActions';
 
 export const ActionCreators = Object.assign({},
   AuthActions,
+  SettingsActions,
 );
